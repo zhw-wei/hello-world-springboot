cf5456b7cbb13bb8f68ed5ba1f6a353d0b7d305a
zhw-wei <2261842924@qq.com>
2021-04-25 11:39:21 +0800

    增加接口测试代码

---
+++ web-api/src/test/java/com/zhw/helloworld/HelloworldControllerTest.java
@@ -0,0 +1,51 @@
+package com.zhw.helloworld;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * @author: zhaohw
+ * @date: 2021.04.25 上午 11:08
+ * @description:
+ */
+@Slf4j
+@SpringBootTest
+public class HelloworldControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+    private MockHttpSession session;
+
+    @BeforeEach
+    public void setUpMockMvc() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        //登录
+        this.session = new MockHttpSession();
+        this.session.setAttribute("user", "user");
+    }
+
+    @Test
+    public void hello() throws Exception {
+
+        int id = 10;
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.get("/hello-world/hello?id=" + id)
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
