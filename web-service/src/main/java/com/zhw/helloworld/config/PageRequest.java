4472d3e9a38029dfc7247284ff9e7720048a82f2
zhw-wei <2261842924@qq.com>
2021-04-26 15:04:13 +0800

    分页查询公共父类

---
+++ web-service/src/main/java/com/zhw/helloworld/config/PageRequest.java
@@ -0,0 +1,16 @@
+package com.zhw.helloworld.config;
+
+import lombok.Data;
+
+/**
+ * @author: zhaohw
+ * @date: 2021.04.26 下午 3:03
+ * @description: 分页查询公共父类
+ */
+@Data
+public class PageRequest {
+
+    private int pageNo = 1;
+
+    private int pageSize = 10;
+}
