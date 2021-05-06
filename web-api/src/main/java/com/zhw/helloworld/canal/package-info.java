f1746ee798c6ec1dfb9094536d3a29f0b73a4d61
zhw-wei <2261842924@qq.com>
2021-05-06 15:39:32 +0800

    增加mq配置

---
+++ web-api/src/main/java/com/zhw/helloworld/canal/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * canal消息处理(实际上也要通过MQ)
+ *
+ * @author: zhaohw
+ * @date: 2021.05.06 下午 3:10
+ */
+package com.zhw.helloworld.canal;
---
+++ web-api/src/main/java/com/zhw/helloworld/mq/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *  MQ消息接收和处理
+ *
+ * @author: zhaohw
+ * @date: 2021.05.06 下午 3:10
+ */
+package com.zhw.helloworld.mq;
