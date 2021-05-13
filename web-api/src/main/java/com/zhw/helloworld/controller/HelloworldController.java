b699405db708e3088d2336819e928ee78ba44b18
zhw-wei <2261842924@qq.com>
2021-05-13 17:26:51 +0800

    增加swagger注解

--- web-api/src/main/java/com/zhw/helloworld/controller/HelloworldController.java
+++ web-api/src/main/java/com/zhw/helloworld/controller/HelloworldController.java
@@ -5,36 +5,45 @@ import com.zhw.helloworld.dal.hello.model.Hello;
 import com.zhw.helloworld.dal.world.model.World;
 import com.zhw.helloworld.hello.service.HelloService;
 import com.zhw.helloworld.world.service.WorldService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.function.Function;
+
 /**
  * @author: zhaohw
  * @date: 2021.04.23 上午 10:18
  * @description:
  */
+@Api(tags = "hello java 接口测试")
 @RestController
 @RequestMapping("/hello-world")
 public class HelloworldController {
+
     @Autowired
     private HelloService helloService;
     @Autowired
     private WorldService worldService;
 
     @GetMapping("/hello")
+    @ApiOperation("hello测试")
     public Result<Hello> hello(@RequestParam Integer id) {
         return this.helloService.hello(id);
     }
 
     @GetMapping("/world")
+    @ApiOperation("world测试")
     public Result<World> world(@RequestParam Integer id) {
         return this.worldService.world(id);
     }
 
     @GetMapping("/transaction")
+    @ApiOperation("transaction测试")
     public Result<Void> transaction() {
         return this.helloService.saveTransaction();
     }
--- web-api/src/main/scala/com/zhw/helloworld/controller/HelloScalaController.scala
+++ web-api/src/main/scala/com/zhw/helloworld/controller/HelloScalaController.scala
@@ -3,6 +3,7 @@ package com.zhw.helloworld.controller
 import com.zhw.helloworld.common.dto.Result
 import com.zhw.helloworld.dal.hello.model.Hello
 import com.zhw.helloworld.hello.service.HelloService
+import io.swagger.annotations.{Api, ApiOperation}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, RequestParam, RestController}
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, Requ
  * @date: 2021.04.30 上午 11:24
  * @description:
  */
+@Api(tags = Array("hello scala 接口测试"))
 @RestController
 @RequestMapping(Array("/hello-scala"))
 class HelloScalaController {
@@ -19,6 +21,7 @@ class HelloScalaController {
   var helloService: HelloService = _
 
   @GetMapping(Array("/hello"))
+  @ApiOperation("hello测试")
   def hello(@RequestParam id: Int): Result[Hello] = {
     helloService.hello(id)
   }
