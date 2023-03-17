ef0c3193af1aba4f3083c54d74de11c4624e035f
zhw-wei <2261842924@qq.com>
2023-03-18 00:21:45 +0800

    新增对接接口

--- web-controller-vue/src/main/java/com/zhw/helloworld/controller/UserInfoController.java
+++ web-controller-vue/src/main/java/com/zhw/helloworld/controller/UserInfoController.java
@@ -20,31 +20,31 @@ public class UserInfoController {
 
 
     @PostMapping("/login")
-    public LoginDto login(@RequestBody LoginDto login){
+    public LoginDto login(@RequestBody LoginDto login) {
 
         //测试使用接口，不执行任何动作
         return login;
     }
 
     @Data
-    public static class LoginDto{
+    public static class LoginDto {
         private String username;
         private String password;
     }
 
     @GetMapping("/studentList")
     public PageResult<StudentInfo> studentList(@RequestParam Integer pageNo,
-                                               @RequestParam Integer pageSize){
+                                               @RequestParam Integer pageSize) {
         List<StudentInfo> list = new ArrayList<>();
         Random random = new Random();
-        for(int i=0; i<pageSize; i++){
+        for (int i = 0; i < pageSize; i++) {
             StudentInfo student = new StudentInfo();
-            student.setId(i+1);
+            student.setId(i + 1);
             student.setName(UUID.randomUUID().toString());
             student.setAge(i + 15);
-            student.setSex(i%2);
-            student.setScore(random.nextInt()%100);
-            student.setClassInfo("班级-" + random.nextInt()%3);
+            student.setSex(i % 2);
+            student.setScore(random.nextInt() % 100);
+            student.setClassInfo("班级-" + random.nextInt() % 3);
 
             list.add(student);
         }
@@ -52,7 +52,7 @@ public class UserInfoController {
     }
 
     @Data
-    public static class StudentInfo{
+    public static class StudentInfo {
         private Integer id;
         private String name;
         private Integer age;
@@ -62,7 +62,30 @@ public class UserInfoController {
     }
 
     @DeleteMapping("/deleteStudent")
-    public Result<Void> deleteStudent(@RequestParam Integer id){
-       return Result.Success.delete();
+    public Result<Void> deleteStudent(@RequestParam Integer id) {
+        return Result.Success.delete();
     }
+
+    @GetMapping("/getStudent")
+    public Result<StudentInfo> getStudent(@RequestParam Integer id) {
+        StudentInfo student = new StudentInfo();
+        student.setId(id);
+        student.setName(UUID.randomUUID().toString());
+        student.setAge(15);
+        student.setSex(1);
+        student.setScore(100);
+        student.setClassInfo("班级-1");
+        return Result.Success.query(student);
+    }
+
+    @PostMapping("/saveStudent")
+    public Result<Void> saveStudent(@RequestBody StudentInfo student) {
+        return Result.Success.add();
+    }
+
+    @PostMapping("/updateStudent")
+    public Result<Void> updateStudent(@RequestBody StudentInfo student) {
+        return Result.Success.update();
+    }
+
 }
